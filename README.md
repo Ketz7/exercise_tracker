9ab83779110f1551c7d8668d4611fbd381336e84
Ketz7 <ketanmaurya@gmail.com>
2022-01-09 05:46:40 +0530

    all tests passing

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # [Exercise Tracker](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker)
+
+This is a Node.js (with Express.js) little application which is part of the FCC Back End Certification. It gives takes an URL and shortens it to a singular digit which can be used to redirect to the original URL.
+
+## Language and Framework
+![Javascript](https://img.shields.io/badge/Language-Javascript-brightgreen) ![Expres](https://img.shields.io/badge/Framework-Express-brightgreen)
+
+___
+## Database
+![MongoDb](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)
+
+___
+## User Stories
+- [x] You can POST to /api/users with form data username to create a new user.
+
+- [x] The returned response from POST /api/users with form data username will be an object with username and _id properties.
+
+- [x] You can make a GET request to /api/users to get a list of all users.
+
+- [x] The GET request to /api/users returns an array.
+
+- [x] Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
+
+- [x] You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+
+- [x] The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+
+- [x] You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
+
+- [x] A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
+
+- [x] A GET request to /api/users/:id/logs will return the user object with a log array of all the exercises added.
+
+- [x] Each item in the log array that is returned from GET /api/users/:id/logs is an object that should have a description, duration, and date properties.
+
+- [x] The description property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.
+
+- [x] The duration property of any object in the log array that is returned from GET /api/users/:id/logs should be a number.
+
+- [x] The date property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.. Use the dateString format of the Date API.
+
+- [x] You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
+
+
+___
+
+Example 
+```
+Post /api/users => {"username":"test","_id":"61da2800c0ff82daa1311915","__v":0}
+____________________________________________________________________________________
+
+Post /api/users/:id/exercises => {"username":"test","description":"Test","date":"Sun Jan 09 2022","_id":"61da2800c0ff82daa1311915","duration":25}
+____________________________________________________________________________________ 
+
+Get /api/users/:id/logs => {
+  "username": "test",
+  "count": 1,
+  "_id": "61da2800c0ff82daa1311915",
+  "log": [
+    {
+      "description": "Test",
+      "duration": 25,
+      "date": "Sun Jan 09 2022"
+    }
+  ]
+}
+____________________________________________________________________________________
+
+GET user's exercise log: GET /api/users/:_id/logs?[from][&to][&limit] => 
+{
+  "username": "test",
+  "count": 1,
+  "_id": "61da2800c0ff82daa1311915",
+  "log": [
+    {
+      "description": "Test",
+      "duration": 25,
+      "date": "Sun Jan 09 2022"
+    }
+  ]
+}
+
+Where limit = 1 || not given
+```
--- server.js
+++ server.js
@@ -1,18 +1,145 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { request, response } = require("express");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+let uri = "<youruri>";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const ExerciseSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+app.post("/api/users", (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+  });
+
+  newUser.save((err, data) => {
+    if (err || !data) {
+      res.send("There was an error saving the user");
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.post("/api/users/:id/exercises", (req, res) => {
+  const id = req.params.id;
+  const { description, duration, date } = req.body;
+  const dateObj = date === "" ? new Date() : new Date(date);
+  let responseObj = {};
+  User.findById(id, (err, userData) => {
+    if (err || !userData) {
+      res.send("Could not find user");
+    } else {
+      const newExercise = new Exercise({
+        userId: id,
+        description,
+        duration: parseInt(duration),
+        date: new Date(dateObj),
+      });
+      if (!newExercise.date) {
+        newExercise.date = new Date().toDateString();
+      } else if (newExercise.date) {
+        newExercise.date = new Date(newExercise.date).toDateString();
+      }
+      newExercise.save((err, data) => {
+        if (err || !data) {
+          res.send("There was an error saving this exercise");
+        } else {
+          let { description, duration, date, _id } = data;
+          let responseObj = {
+            username: userData.username,
+            description,
+            date: newExercise.date.toDateString(),
+            _id: userData.id,
+            duration,
+          };
+          res.json(responseObj);
+        }
+      });
+    }
+  });
+});
+
+app.get("/api/users/:id/logs", (req, res) => {
+  if (req.query.limit == undefined || "") {
+    req.query["limit"] = 500;
+  }
+  const { from, to, limit } = req.query;
+  const { id } = req.params;
+  User.findById(id, (err, userData) => {
+    if (err || !userData) {
+      res.send("Could not find User");
+    } else {
+      let dateObj = {};
+      if (from) {
+        dateObj["$gte"] = new Date(from);
+      }
+      if (to) {
+        dateObj["$lte"] = new Date(to);
+      }
+      let filter = {
+        userId: id,
+      };
+      if (from || to) {
+        filter.date = dateObj;
+      }
+      let nonNullLimit = limit;
+      Exercise.find(filter)
+        .limit(+nonNullLimit)
+        .exec((err, data) => {
+          if (err || !data) {
+            res.json({});
+          } else {
+            const count = data.length;
+            const rawLog = data;
+            const { username, _id } = userData;
+            const log = rawLog.map((l) => ({
+              description: l.description,
+              duration: l.duration,
+              date: new Date(l.date).toDateString(),
+            }));
+            res.json({ username, count, _id, log });
+          }
+        });
+    }
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  User.find({}, (err, data) => {
+    if (err || !data) {
+      res.send("No users");
+    } else {
+      res.json(data);
+    }
+  });
+});
